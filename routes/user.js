30d8716af85f05cfb702f4c924c55a4162692c4d
Revan Kumar <revan01.g@gmail.com>
2025-07-10 15:00:14 +0530

    Restore application to working state

--- routes/user.js
+++ routes/user.js
@@ -17,6 +17,30 @@ router.route('/login')
         passport.authenticate('local', { failureRedirect: '/login', failureFlash: true}),
         UserController.UserLogIn);
 
+router.post('/login', (req, res, next) => {
+    console.log('Login attempt for:', req.body.username);
+    passport.authenticate('local', (err, user, info) => {
+        if (err) {
+            console.log('Login error:', err);
+            return next(err);
+        }
+        if (!user) {
+            console.log('Login failed:', info.message);
+            req.flash('error', info.message);
+            return res.redirect('/login');
+        }
+        req.logIn(user, (err) => {
+            if (err) {
+                console.log('Login session error:', err);
+                return next(err);
+            }
+            console.log('Login successful for:', user.username);
+            req.flash('success', 'Welcome back!');
+            return res.redirect('/listings');
+        });
+    })(req, res, next);
+});
+
 router.get('/logout',wrapAsync(UserController.UserLogOut));
 
 module.exports=router;
