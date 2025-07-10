85b2b6ccc78523953e68788c674c6e527523b8ec
Revan Kumar <revan01.g@gmail.com>
2025-07-10 13:33:20 +0530

    Ready for deployment

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.set('views',path.join(__dirname,'views'));
 app.use(express.static(path.join(__dirname,'public')));
 
 const sessionOptions={
-    secret: process.env.SESSION_SECRET || process.env.SECRET || 'mysecretcode',
+    secret: process.env.SESSION_SECRET || 'mysecretcode',
     resave:false,
     saveUninitialized:true,
     cookie:{
